a2978a6b04a394a1e3bcf5051dd2af0b03119669
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2025-01-22 01:35:33 +0900

    Make class

--- examples/pitcher.py
+++ examples/pitcher.py
@@ -1,12 +1,10 @@
 from xml.etree.ElementTree import tostring
 from lxml import etree
 import ycb_utils
-from mujoco_xml_utils.xml_utils import _convex_decomposition, create_empty_xml, add_mesh
+from mujoco_xml_utils.xml_utils import MujocoXmlEditor
 
+editor = MujocoXmlEditor.empty("example")
 mesh_path = ycb_utils.resolve_path("019_pitcher_base")
-xml = create_empty_xml(mesh_path, "example")
-add_mesh(xml, mesh_path, "pitcher_base", convex_decomposition=True)
-xmlstr = tostring(xml, encoding="utf8")
-et = etree.fromstring(xmlstr)
-pretty_xml = etree.tostring(et, pretty_print=True, encoding=str)
-print(pretty_xml)
+editor.add_mesh(mesh_path, "pitcher_base", convex_decomposition=True)
+xmlstr = editor.to_string()
+print(xmlstr)
--- mujoco_xml_utils/xml_utils.py
+++ mujoco_xml_utils/xml_utils.py
@@ -1,4 +1,5 @@
 import trimesh
+from lxml import etree
 import numpy as np
 from pathlib import Path
 from trimesh import Trimesh
@@ -7,75 +8,93 @@ import coacd
 from xml.etree.ElementTree import Element, SubElement, tostring
 
 
-def _mesh_hash(mesh: Trimesh):
-    return md5(mesh.vertices.tobytes() + mesh.faces.tobytes()).hexdigest()
+class MujocoXmlEditor:
+    def __init__(self, root: Element):
+        self.root = root
 
+    @classmethod
+    def empty(cls, model_name: str):
+        root = Element("mujoco")
+        root.set("model", model_name)
+        return cls(root)
 
-def _convex_decomposition(mesh: Trimesh, threshold: float) -> Path:
-    coacd_mesh = coacd.Mesh(mesh.vertices, mesh.faces)
-    dir_name = _mesh_hash(mesh) + f"threshold_{threshold}"
-    cache_base_path = Path("~/.cache/mujoco_xml_utils/convex_decomposition").expanduser() / dir_name
-    if not cache_base_path.exists():
-        cache_base_path.mkdir(parents=True, exist_ok=True)
-        parts = coacd.run_coacd(coacd_mesh, threshold=threshold)
-        for i, (V, F) in enumerate(parts):
-            part = Trimesh(V, F)
-            file_path = cache_base_path / f"part_{i}.obj"
-            part.export(file_path)
-    return cache_base_path
+    @classmethod
+    def load(cls, file_path: Path):
+        return cls(root=ElementTree.parse(file_path).getroot())
 
+    def to_string(self) -> str:
+        xmlstr = tostring(self.root, encoding="utf8")
+        et = etree.fromstring(xmlstr)
+        pretty_xml = etree.tostring(et, pretty_print=True, encoding=str)
+        return pretty_xml
 
-def _create_element_if_not_exists(root: Element, tag: str) -> Element:
-    element = root.find(tag)
-    if element is None:
-        element = SubElement(root, tag)
-    return element
+    def add_mesh(
+        self,
+        mesh_file: Path,
+        name: str,
+        convex_decomposition: bool = True,
+        cd_threshold=0.02,
+        pos: np.ndarray = np.zeros(3),
+        euler: np.ndarray = np.zeros(3),
+    ) -> None:
+        # create asset
+        asset = self._create_element_if_not_exists(self.root, "asset")
+        if convex_decomposition:
+            mesh = trimesh.load(mesh_file)
+            cache_path = self._convex_decomposition(mesh, cd_threshold)
+            assert len(list(cache_path.iterdir())) > 0
+            for i, part_file in enumerate(cache_path.iterdir()):
+                part_name = f"{name}_part_{i}"
+                mesh = SubElement(asset, "mesh", {"file": str(part_file), "name": part_name})
+        else:
+            mesh = SubElement(asset, "mesh", {"file": str(mesh_file), "name": name})
 
-
-def add_mesh(
-    root: Element,
-    mesh_file: Path,
-    name: str,
-    convex_decomposition: bool = True,
-    cd_threshold=0.02,
-    pos: np.ndarray = np.zeros(3),
-    euler: np.ndarray = np.zeros(3),
-) -> None:
-    # create asset
-    asset = _create_element_if_not_exists(root, "asset")
-    if convex_decomposition:
-        mesh = trimesh.load(mesh_file)
-        cache_path = _convex_decomposition(mesh, cd_threshold)
-        assert len(list(cache_path.iterdir())) > 0
-        for i, part_file in enumerate(cache_path.iterdir()):
-            part_name = f"{name}_part_{i}"
-            mesh = SubElement(asset, "mesh", {"file": str(part_file), "name": part_name})
-    else:
-        mesh = SubElement(asset, "mesh", {"file": str(mesh_file), "name": name})
-
-    # create body
-    worldbody = _create_element_if_not_exists(root, "worldbody")
-    body = SubElement(
-        worldbody,
-        "body",
-        attrib={
-            "name": name,
-            "pos": " ".join(map(str, pos)),
-            "euler": " ".join(map(str, euler)),
-        },
-    )
-    SubElement(body, "joint", attrib={"name": "joint_" + name, "type": "free"})
-    if convex_decomposition:
-        for i in range(len(list(cache_path.iterdir()))):
-            part_name = f"{name}_part_{i}"
+        # create body
+        worldbody = self._create_element_if_not_exists(self.root, "worldbody")
+        body = SubElement(
+            worldbody,
+            "body",
+            attrib={
+                "name": name,
+                "pos": " ".join(map(str, pos)),
+                "euler": " ".join(map(str, euler)),
+            },
+        )
+        SubElement(body, "joint", attrib={"name": "joint_" + name, "type": "free"})
+        if convex_decomposition:
+            for i in range(len(list(cache_path.iterdir()))):
+                part_name = f"{name}_part_{i}"
+                geom = SubElement(
+                    body, "geom", attrib={"mesh": part_name, "type": "mesh", "density": "1000"}
+                )
+        else:
             geom = SubElement(
-                body, "geom", attrib={"mesh": part_name, "type": "mesh", "density": "1000"}
+                body, "geom", attrib={"mesh": name, "type": "mesh", "density": "1000"}
             )
-    else:
-        geom = SubElement(body, "geom", attrib={"mesh": name, "type": "mesh", "density": "1000"})
 
+    @staticmethod
+    def _mesh_hash(mesh: Trimesh):
+        return md5(mesh.vertices.tobytes() + mesh.faces.tobytes()).hexdigest()
 
-def create_empty_xml(file_path: Path, model_name: str) -> Element:
-    root = Element("mujoco")
-    root.set("model", model_name)
-    return root
+    @staticmethod
+    def _convex_decomposition(mesh: Trimesh, threshold: float) -> Path:
+        coacd_mesh = coacd.Mesh(mesh.vertices, mesh.faces)
+        dir_name = MujocoXmlEditor._mesh_hash(mesh) + f"threshold_{threshold}"
+        cache_base_path = (
+            Path("~/.cache/mujoco_xml_utils/convex_decomposition").expanduser() / dir_name
+        )
+        if not cache_base_path.exists():
+            cache_base_path.mkdir(parents=True, exist_ok=True)
+            parts = coacd.run_coacd(coacd_mesh, threshold=threshold)
+            for i, (V, F) in enumerate(parts):
+                part = Trimesh(V, F)
+                file_path = cache_base_path / f"part_{i}.obj"
+                part.export(file_path)
+        return cache_base_path
+
+    @staticmethod
+    def _create_element_if_not_exists(parent: Element, tag: str) -> Element:
+        element = parent.find(tag)
+        if element is None:
+            element = SubElement(parent, tag)
+        return element

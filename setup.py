6cc924184f298726b9bfc7bedbdbd6b8b09aa82b
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2025-01-23 06:30:33 +0900

    setup.py based intsall for better IDE support

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="mujoco-xml-editor",
+    version="0.0.0",
+    packages=["mujoco_xml_editor"],
+    install_requires=[
+        "numpy",
+        "coacd",
+        "lxml",
+    ],
+    extras_require={
+        "examples": [
+            "mujoco",
+            "mujoco-python-viewer",
+            "ycb-utils",
+            "robot_descriptions",
+        ]
+    },
+    package_data={"mujoco": ["py.typed"]},
+)
